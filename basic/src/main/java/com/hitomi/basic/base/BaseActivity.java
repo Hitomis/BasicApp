aa652c00866dfd53ed0736c1f3b7f8845efceb3d
hitomi <196425254@qq.com>
2016-12-11 12:26:36 +0800

    Initial commit

---
+++ basic/src/main/java/com/hitomi/basic/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.hitomi.basic.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hitomi on 2016/12/11.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+
+}
