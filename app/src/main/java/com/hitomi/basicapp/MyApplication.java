a1462dd6c0bb87b306fa81fcfa0289e20158d64c
hitomi <196425254@qq.com>
2016-12-11 17:00:55 +0800

    Add LeakCanary

--- app/src/main/java/com/hitomi/basicapp/MyApplication.java
+++ app/src/main/java/com/hitomi/basicapp/MyApplication.java
@@ -3,6 +3,7 @@ package com.hitomi.basicapp;
 import android.app.Application;
 
 import com.hitomi.basic.manager.ActivityManager;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by hitomi on 2016/12/11.
@@ -12,7 +13,10 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
+        }
         registerActivityLifecycleCallbacks(ActivityManager.getInstance());
+
     }
 }
