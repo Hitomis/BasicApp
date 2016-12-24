78e414e7909179aba91bbf942c4b68df056405b5
hitomi <196425254@qq.com>
2016-12-24 12:27:43 +0800

    Complete code

--- app/src/main/java/com/hitomi/basicapp/MyApplication.java
+++ app/src/main/java/com/hitomi/basicapp/MyApplication.java
@@ -33,6 +33,9 @@ public class MyApplication extends Application {
         initSlideback();
     }
 
+    /**
+     * 初始化 Activity 栈管理器
+     */
     private void initActivityManager() {
         ActivityManager activityManager = ActivityManager.getInstance();
         activityManager.init(this);
@@ -40,6 +43,9 @@ public class MyApplication extends Application {
         activityManager.addIgnor("DisplayLeakActivity");
     }
 
+    /**
+     * 初始化侧滑返回组件
+     */
     private void initSlideback() {
         SlideConfig slideConfig = new SlideConfig.Builder()
                 .rotateScreen(false) // 屏幕是否旋转
@@ -48,9 +54,12 @@ public class MyApplication extends Application {
                 .edgePercent(0.1f) // 边缘滑动的响应阈值，0~1，对应屏幕宽度*percent
                 .slideOutPercent(0.5f) // 关闭页面的阈值，0~1，对应屏幕宽度*percent
                 .create();
-        SlideBackHelper.getInstance().init(slideConfig);
+        SlideBackHelper.getInstance().init(ActivityManager.getInstance(), slideConfig);
     }
 
+    /**
+     * 初始化 XLog 日志管理器
+     */
     private void initXLog() {
         LogConfiguration config = new LogConfiguration.Builder()
                 .logLevel(BuildConfig.DEBUG
